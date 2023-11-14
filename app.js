9bbf7e441b9ae3dae6861077e93c9a64fb2e014e
Fatmatrn <ftmtrn3488@gmail.com>
2023-11-14 22:55:52 +0300

    p

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express();
 
+//Templetate Engine
+app.set('view engine', 'ejs');
+
+//Body Parser Middleware
+app.use(express.urlencoded({ extended: true }));
+
+//Static Files
+app.use(express.static("public"));
+
+//Routes
 
 app.get('/',(req,res)=>{
-    res.send('index sayfasi')
-})
-
+    res.status(200).render('index')
+});
+app.get('/about',(req,res)=>{
+    res.status(200).render('about')
+});
 
 
 
