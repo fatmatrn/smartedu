7401d892a3a63dd448693368a48bb5e58bde8921
Fatmatrn <ftmtrn3488@gmail.com>
2023-12-06 19:12:02 +0300

    getcourse

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -15,3 +15,54 @@ exports.createCourse = async (req, res) => {
         });
     }
 }
+//MVC  yapisinda view olarak donen version
+//get all coursse
+exports.getAllCourse = async (req, res) => {
+    try {
+        const courses = await Course.find(req.body);
+
+        res.status(201).render('courses',{//view in icindeki courses.ejs yi render et, ve oraya yakalamis oldugum kurslari gondereceksin
+            courses,
+            page_name:'courses'//ayrica view e bir page_name gondererek tiklandiginda active ye sahip olacak  olan sayfayi belirlemis oluyoruz
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error
+        });
+    }
+}
+
+//json donen version
+// exports.getAllCourse = async (req, res) => {
+//     try {
+//         const courses = await Course.find(req.body);
+
+//         res.status(201).json({
+//             status: 'success',
+//             courses
+//         });
+//     } catch (error) {
+//         res.status(400).json({
+//             status: 'fail',
+//             error
+//         });
+//     }
+// }
+
+//get a courses
+exports.getCourse = async (req, res) => {
+    try {
+        const course = await Course.findById({_id: req.params.id});//burada url deki id yi yakaliyoruz
+
+        res.status(201).render('course',{//view in icindeki courses.ejs yi render et, ve oraya yakalamis oldugum kurslari gondereceksin
+            course,
+            page_name:'courses'//ayrica view e bir page_name gondererek tiklandiginda active ye sahip olacak  olan sayfayi belirlemis oluyoruz
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error
+        });
+    }
+}
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -3,7 +3,9 @@ const courseController = require('../controllers/courseController')
 
 const router = express.Router()
 
-router.route('/').post(courseController.createCourse);  //http://localhost:3000/course
+router.route('/').post(courseController.createCourse);  //http://localhost:3000/courses
+router.route('/').get(courseController.getAllCourse);  //http://localhost:3000/courses
+router.route('/:id').get(courseController.getCourse);  //http://localhost:3000/courses/1
 
 
 module.exports=router;
