24b1ba33385f21ec0ac637b23e7e352347c55a02
Fatmatrn <ftmtrn3488@gmail.com>
2023-12-17 22:10:19 +0300

    mail

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require("nodemailer");
+
 exports.getIndexPage=(req,res)=>{
     console.log(req.session.userID);
     res.status(200).render('index',{
@@ -27,5 +29,62 @@ exports.getContactPage=(req,res)=>{
     })
 };
 exports.sendEmail=(req,res)=>{
-    console.log(req,body)
+    //burada html kodu yazacagimiz icin backtick kullaniyoruz
+    const outputMessage =`
+    <h1>Mail Details</h1>
+    <ul style="list-style-type: none">
+    <li>Name: ${req.body.name} </li>
+    <li>Mail: ${req.body.email}</li>
+  </ul>
+  <h1>Message</h1>
+  <p>${req.body.message}</p>
+
+    `
+   
+
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+          user: "",
+          pass: "REPLACE-WITH-YOUR-GENERATED-PASSWORD",
+        },
+      });
+      
+      // async..await is not allowed in global scope, must use a wrapper
+      async function main() {
+        // send mail with defined transport object
+        const info = await transporter.sendMail({
+          from: '"Fred Foo 👻" <foo@example.com>', // sender address
+          to: "bar@example.com, baz@example.com", // list of receivers
+          subject: "Hello ✔", // Subject line
+          text: "Hello world?", // plain text body
+          html: "<b>Hello world?</b>", // html body
+        });
+      
+        console.log("Message sent: %s", info.messageId);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      
+        //
+        // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+        //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+        //       <https://github.com/forwardemail/preview-email>
+        //
+      }
+      
+      main().catch(console.error);
+
 };
+
+
+
+
+
+
+
+
+
+
+
